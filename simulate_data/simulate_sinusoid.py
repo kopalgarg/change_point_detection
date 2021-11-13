33155c088949ed76bd9924bf2cc115448677e307
kopalgarg <kopalgarg24@gmail.com>
2021-11-13 09:31:15 -0500

    add code to plot sample signal

--- simulate_data/simulate_sinusoid.py
+++ simulate_data/simulate_sinusoid.py
@@ -17,13 +17,21 @@ def main(type, num_samples, seq_length):
     print(type, 'saved')
     signal_in = np.array(signal_in)
 
+    # split 70% train 30% test
     x_train, x_test = train_test_split(signal_in, test_size=0.30, random_state=42)
     
-    return x_train, x_test
+    # print shape
+    print("x_train shape", x_train.shape)
+    print("x_test shape", x_test.shape)
+
+    # save a sample plot
+    plt.plot(x_train[0])
+    
+    return x_train, x_test, plt
 
 def irregularly_sampled_sinusoid(seq_length):
 
-    white_noise = ts.noise.GaussianNoise(std=0.01)
+    white_noise = ts.noise.GaussianNoise(std=0.3)
     sinusoid = ts.signals.Sinusoidal(frequency=0.25)
     time_series = ts.TimeSeries(sinusoid, noise_generator=white_noise)
     sample, signals, error = time_series.sample(np.array(range(seq_length)))
@@ -38,14 +46,16 @@ def save_data(path,array):
 if __name__=='__main__':
     if not os.path.exists('./data'):
         os.mkdir('./data')
-    n_samples = 10
-    s_len = 1000
+    n_samples = 10 # samples per unit
+    s_len = 50 # seq length
+
     type = 'irregularly_sampled_sinusoid'
-    x_train, x_test = main(type = type, num_samples=n_samples,seq_length=s_len)
+    x_train, x_test, plt = main(type = type, num_samples= n_samples, seq_length = s_len)
     
     path = os.path.join('./data/', type)
     if not os.path.exists(path):
         os.mkdir(path)
-
+    
     save_data(os.path.join(path, 'x_train.pkl'), x_train)
     save_data(os.path.join(path, 'x_test.pkl'), x_test)
+    plt.savefig(os.path.join(path, 'sample_signal.png'))
