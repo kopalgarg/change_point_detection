fd878ef27a45d479ff30de753379eab3bfdbe6b7
kopalgarg <kopalgarg24@gmail.com>
2021-11-15 16:07:37 -0500

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 13 2021
+@author: Kopal Garg
+"""
+import numpy as np
+import pandas as pd
+
+# mean
+def mean():
+    return 0
+
+# variance
+def variance():
+    return 0
+
+# wasserstein distance
+def wasst_dist():
+    return 0
+
+# univariate 2 sample test
+def univariate_2sample():
+    return 0
+
+# multivariate 2 sample test
+def multivariate_2sample():
+    return 0
+
+# correlation
+def correlation():
+    return 0
